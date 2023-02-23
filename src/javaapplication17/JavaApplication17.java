756187e3d8332882112902e5f512b849c5d134c1
Marc Raineer Filosopo <marcraineer0089@gmail.com>
2023-02-23 10:50:30 +0800

    Initial Commit

---
+++ src/javaapplication17/JavaApplication17.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication17;
+
+/**
+ *
+ * @author JSMJC-SH
+ */
+public class JavaApplication17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    
+    public static void main(String[] args) {
+           MainForm a = new MainForm();
+           a.show();
+    }
+}
